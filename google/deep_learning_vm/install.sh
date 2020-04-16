a1385de94c4f7b044841b2a23620384af080edc3
Bradley Arsenault <brad@swiftlyai.com>
2020-04-16 11:48:49 -0400

    Another small fix to the google cloud vm build script.

--- google/deep_learning_vm/install.sh
+++ google/deep_learning_vm/install.sh
@@ -26,6 +26,8 @@ echo "Downloading and installing Chromedriver"
 wget https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 sudo cp chromedriver /usr/bin/
+sudo chmod 644 /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
 rm -rf chromedriver
 rm -rf chromedriver_linux64.zip
 
